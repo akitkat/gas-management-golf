ad924193f3fc2176fe146c16fe68ae3aea1ee1ef
Naoya Akimoto <akitkat.cook.dx@gmail.com>
2021-09-05 22:30:21 +0900

    add base spreadsheet.

---
+++ src/infrastructure/spreadsheet/baseSpreadSheet.js
@@ -0,0 +1,54 @@
+export default class {
+  sheet;
+  sheetName;
+  spreadSheet;
+
+  constructor(sheetName) {
+    this.sheetName = sheetName;
+    this.spreadSheet = SpreadsheetApp.getActiveSpreadsheet();
+    this.sheet = this.spreadSheet.getSheetByName(this.sheetName);
+  }
+
+  selectAll() {
+    if (this.sheet.getLastRow() === 1) {
+      return [];
+    }
+
+    return this.sheet
+      .getRange(2, 1, this.sheet.getLastRow() - 1, this.sheet.getLastColumn())
+      .getValues();
+  }
+
+  replceAll(data) {
+    this.clearExceptHeader();
+
+    const rows = data.map((e) => {
+      return e.toArray();
+    });
+
+    this.sheet.getRange(2, 1, rows.length, rows[0].length).setValues(rows);
+  }
+
+  insertAll(data) {
+    const rows = data.map((e) => {
+      return e.toArray();
+    });
+
+    this.sheet
+      .getRange(
+        this.sheet.getLastRow() + 1,
+        1,
+        rows.length,
+        this.sheet.getLastColumn()
+      )
+      .setValues(rows);
+  }
+
+  clearExceptHeader() {
+    if (0 < this.sheet.getLastRow()) {
+      this.sheet
+        .getRange(2, 1, this.sheet.getLastRow(), this.sheet.getLastColumn())
+        .clear();
+    }
+  }
+}
