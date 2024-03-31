42c6e26e76bfc5d2760d5ccf76a37cac66261b29
akitkat <akitkat.cook.dx@gmail.com>
2024-03-31 10:24:11 +0900

    fix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import doGet from './func/doGet';
 import fetchAllGolfCourse from './func/fetchAllGolfCourse';
+import fetchAllCourseDetail from './func/fetch_all_course_detail';
 import onOpen from './func/onOpen';
 
 global.doGet = (e) => doGet(e);
 global.fetchAllGolfCourse = () => fetchAllGolfCourse();
+global.fetchAllCourseDetail = () => fetchAllCourseDetail();
 global.onOpen = (e) => onOpen(e);
