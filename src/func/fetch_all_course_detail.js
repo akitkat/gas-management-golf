7315d0203c227783705508da8082d65363b14583
akitkat <akitkat.cook.dx@gmail.com>
2024-04-01 21:34:09 +0900

    Add fetch_all_course_detail function

---
+++ src/func/fetch_all_course_detail.js
@@ -0,0 +1,20 @@
+import CourseDetailApi from "../infrastructure/api/rakuten/course_detail";
+import CourseDetailSheet from "../infrastructure/spreadsheet/course_detail_sheet";
+
+export default () => {
+  let i = 0;
+
+  const courseDetails = new CourseDetailSheet().fetchAll();
+  const replaceCourseDetails = courseDetails.map((courseDetail) => {
+    if (courseDetail.golfCourseName !== "" ) {
+      return courseDetail;
+    }
+
+    if (100 <= i) {
+      return courseDetail;
+    }
+
+    i++;
+    return new CourseDetailApi().fetch(courseDetail.golfCourseId);
+  });
+};
