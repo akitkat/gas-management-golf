befcae29f10d71fa0ed7a674107b32450b8d95a6
akitkat <akitkat.cook.dx@gmail.com>
2024-04-06 16:35:28 +0900

    Add fetchAllJalanCourseDetail function to fetch and replace all course details from Jalan API

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import doGet from './func/doGet';
 import fetchAllGolfCourse from './func/fetchAllGolfCourse';
 import fetchAllCourseDetail from './func/fetch_all_course_detail';
+import fetchAllJalanCourseDetail from './func/fetch_all_jalan_course_detail';
 import onOpen from './func/onOpen';
 
 global.doGet = (e) => doGet(e);
 global.fetchAllGolfCourse = () => fetchAllGolfCourse();
 global.fetchAllCourseDetail = () => fetchAllCourseDetail();
+global.fetchAllJalanCourseDetail = () => fetchAllJalanCourseDetail();
 global.onOpen = (e) => onOpen(e);
