0d66e9ad033e391944ddc5d8bb841f08c288e477
akitkat <akitkat.cook.dx@gmail.com>
2024-04-29 18:26:44 +0900

    Refactor JalanCourseDetailApi class to fix bug in fetch method

---
+++ src/models/export_course_detail.js
@@ -0,0 +1,457 @@
+export default class ExportCourseDetail {
+  constructor(
+    golfCourseId,
+    golfCourseName,
+    golfCourseCaption,
+    information,
+    highway,
+    ic,
+    postalCode,
+    address,
+    telephoneNo,
+    closeDay,
+    creditCard,
+    shoes,
+    dressCode,
+    practiceFacility,
+    lodgingFacility,
+    otherFacility,
+    golfCourseImageUrl1,
+    weekdayMinPrice,
+    baseWeekdayMinPrice,
+    holidayMinPrice,
+    baseHolidayMinPrice,
+    designer,
+    courseType,
+    courseVerticalInterval,
+    dimension,
+    green,
+    greenCount,
+    holeCount,
+    parCount,
+    ratingNum,
+    evaluation,
+    staff,
+    facility,
+    meal,
+    course,
+    costperformance,
+    distance,
+    fairway,
+    clubCd,
+    acareUsable,
+    accessBus,
+    accessCar,
+    accessTrain,
+    approvalContent,
+    approvalDate,
+    approvalFlg,
+    bankAccountFlg,
+    cancelFeeDayHoliday,
+    cancelFeeDayUsual,
+    cancelFeeFlg,
+    cancelNote,
+    card,
+    cardNote,
+    cartNote,
+    catchCopy,
+    chargeName,
+    chargeSCd,
+    chgDedlineDayReal,
+    chgDedlineDayReq,
+    chgDedlineHmReal,
+    chgDedlineHmReq,
+    clubRegistDatetime,
+    clubUpdateDatetime,
+    cnlDedlineDayReal,
+    cnlDedlineDayReq,
+    cnlDedlineHmReal,
+    cnlDedlineHmReq,
+    compeRoom,
+    compeRoomNote,
+    contractFlg,
+    coreSystemId,
+    couponClubUsable,
+    couponRUsable,
+    courseCount,
+    courseDesigner,
+    coursePresentation,
+    courseSize,
+    createModuleCd,
+    createUserCd,
+    createdAt,
+    deleted,
+    delivery,
+    distanceUnit,
+    drconHole,
+    dress,
+    faxNo,
+    gdoFrameFlg,
+    gdoPlanFlg,
+    greenBent,
+    greenKorai,
+    greenOther,
+    holeCountAll,
+    hotSpring,
+    hotSpringNote,
+    hotel,
+    id,
+    jalanInformation,
+    inquiryEigyoCd,
+    kana,
+    latitude,
+    longitude,
+    maxTeeCount,
+    name,
+    nrpinHole,
+    openFlg,
+    openPlanFlg,
+    openPlanMonthHoliday,
+    openPlanMonthHolidayFlg,
+    openPlanMonthSatday,
+    openPlanMonthSatdayFlg,
+    openPlanMonthUsual,
+    openPlanMonthUsualFlg,
+    openPlanTimerFlg,
+    openYmd,
+    parCountAll,
+    photoGalleryOpenFlg,
+    pointUpPostingStatus,
+    pointUsable,
+    ponpareUsable,
+    practiceSpace,
+    practiceSpaceNote,
+    prefectureId,
+    priceLowerLimitHoliday,
+    priceLowerLimitWeekday,
+    rentalClub,
+    rentalClubNote,
+    rentalShoesNote,
+    reserveMaxParty,
+    restaurantNote,
+    roundStyleNote,
+    rsvDedlineDayReal,
+    rsvDedlineDayReq,
+    rsvDedlineHmReal,
+    rsvDedlineHmReq,
+    sCd,
+    seq,
+    shoesNote,
+    stayPlanUsable,
+    telNo,
+    testClient,
+    tlPriceLowerLimitHoliday,
+    tlPriceLowerLimitWeekday,
+    tourismClubCd,
+    updateModuleCd,
+    updateUserCd,
+    updatedAt,
+    version,
+    verticalInterval,
+    yardCountAll
+  ) {
+    this.golfCourseId = golfCourseId;
+    this.golfCourseName = golfCourseName;
+    this.golfCourseCaption = golfCourseCaption;
+    this.information = information;
+    this.highway = highway;
+    this.ic = ic;
+    this.postalCode = postalCode;
+    this.address = address;
+    this.telephoneNo = telephoneNo;
+    this.closeDay = closeDay;
+    this.creditCard = creditCard;
+    this.shoes = shoes;
+    this.dressCode = dressCode;
+    this.practiceFacility = practiceFacility;
+    this.lodgingFacility = lodgingFacility;
+    this.otherFacility = otherFacility;
+    this.golfCourseImageUrl1 = golfCourseImageUrl1;
+    this.weekdayMinPrice = weekdayMinPrice;
+    this.baseWeekdayMinPrice = baseWeekdayMinPrice;
+    this.holidayMinPrice = holidayMinPrice;
+    this.baseHolidayMinPrice = baseHolidayMinPrice;
+    this.designer = designer;
+    this.courseType = courseType;
+    this.courseVerticalInterval = courseVerticalInterval;
+    this.dimension = dimension;
+    this.green = green;
+    this.greenCount = greenCount;
+    this.holeCount = holeCount;
+    this.parCount = parCount;
+    this.ratingNum = ratingNum;
+    this.evaluation = evaluation;
+    this.staff = staff;
+    this.facility = facility;
+    this.meal = meal;
+    this.course = course;
+    this.costperformance = costperformance;
+    this.distance = distance;
+    this.fairway = fairway;
+    this.clubCd = clubCd;
+    this.acareUsable = acareUsable;
+    this.accessBus = accessBus;
+    this.accessCar = accessCar;
+    this.accessTrain = accessTrain;
+    this.approvalContent = approvalContent;
+    this.approvalDate = approvalDate;
+    this.approvalFlg = approvalFlg;
+    this.bankAccountFlg = bankAccountFlg;
+    this.cancelFeeDayHoliday = cancelFeeDayHoliday;
+    this.cancelFeeDayUsual = cancelFeeDayUsual;
+    this.cancelFeeFlg = cancelFeeFlg;
+    this.cancelNote = cancelNote;
+    this.card = card;
+    this.cardNote = cardNote;
+    this.cartNote = cartNote;
+    this.catchCopy = catchCopy;
+    this.chargeName = chargeName;
+    this.chargeSCd = chargeSCd;
+    this.chgDedlineDayReal = chgDedlineDayReal;
+    this.chgDedlineDayReq = chgDedlineDayReq;
+    this.chgDedlineHmReal = chgDedlineHmReal;
+    this.chgDedlineHmReq = chgDedlineHmReq;
+    this.clubRegistDatetime = clubRegistDatetime;
+    this.clubUpdateDatetime = clubUpdateDatetime;
+    this.cnlDedlineDayReal = cnlDedlineDayReal;
+    this.cnlDedlineDayReq = cnlDedlineDayReq;
+    this.cnlDedlineHmReal = cnlDedlineHmReal;
+    this.cnlDedlineHmReq = cnlDedlineHmReq;
+    this.compeRoom = compeRoom;
+    this.compeRoomNote = compeRoomNote;
+    this.contractFlg = contractFlg;
+    this.coreSystemId = coreSystemId;
+    this.couponClubUsable = couponClubUsable;
+    this.couponRUsable = couponRUsable;
+    this.courseCount = courseCount;
+    this.courseDesigner = courseDesigner;
+    this.coursePresentation = coursePresentation;
+    this.courseSize = courseSize;
+    this.createModuleCd = createModuleCd;
+    this.createUserCd = createUserCd;
+    this.createdAt = createdAt;
+    this.deleted = deleted;
+    this.delivery = delivery;
+    this.distanceUnit = distanceUnit;
+    this.drconHole = drconHole;
+    this.dress = dress;
+    this.faxNo = faxNo;
+    this.gdoFrameFlg = gdoFrameFlg;
+    this.gdoPlanFlg = gdoPlanFlg;
+    this.greenBent = greenBent;
+    this.greenKorai = greenKorai;
+    this.greenOther = greenOther;
+    this.holeCountAll = holeCountAll;
+    this.hotSpring = hotSpring;
+    this.hotSpringNote = hotSpringNote;
+    this.hotel = hotel;
+    this.id = id;
+    this.jalanInformation = jalanInformation;
+    this.inquiryEigyoCd = inquiryEigyoCd;
+    this.kana = kana;
+    this.latitude = latitude;
+    this.longitude = longitude;
+    this.maxTeeCount = maxTeeCount;
+    this.name = name;
+    this.nrpinHole = nrpinHole;
+    this.openFlg = openFlg;
+    this.openPlanFlg = openPlanFlg;
+    this.openPlanMonthHoliday = openPlanMonthHoliday;
+    this.openPlanMonthHolidayFlg = openPlanMonthHolidayFlg;
+    this.openPlanMonthSatday = openPlanMonthSatday;
+    this.openPlanMonthSatdayFlg = openPlanMonthSatdayFlg;
+    this.openPlanMonthUsual = openPlanMonthUsual;
+    this.openPlanMonthUsualFlg = openPlanMonthUsualFlg;
+    this.openPlanTimerFlg = openPlanTimerFlg;
+    this.openYmd = openYmd;
+    this.parCountAll = parCountAll;
+    this.photoGalleryOpenFlg = photoGalleryOpenFlg;
+    this.pointUpPostingStatus = pointUpPostingStatus;
+    this.pointUsable = pointUsable;
+    this.ponpareUsable = ponpareUsable;
+    this.practiceSpace = practiceSpace;
+    this.practiceSpaceNote = practiceSpaceNote;
+    this.prefectureId = prefectureId;
+    this.priceLowerLimitHoliday = priceLowerLimitHoliday;
+    this.priceLowerLimitWeekday = priceLowerLimitWeekday;
+    this.rentalClub = rentalClub;
+    this.rentalClubNote = rentalClubNote;
+    this.rentalShoesNote = rentalShoesNote;
+    this.reserveMaxParty = reserveMaxParty;
+    this.restaurantNote = restaurantNote;
+    this.roundStyleNote = roundStyleNote;
+    this.rsvDedlineDayReal = rsvDedlineDayReal;
+    this.rsvDedlineDayReq = rsvDedlineDayReq;
+    this.rsvDedlineHmReal = rsvDedlineHmReal;
+    this.rsvDedlineHmReq = rsvDedlineHmReq;
+    this.sCd = sCd;
+    this.seq = seq;
+    this.shoesNote = shoesNote;
+    this.stayPlanUsable = stayPlanUsable;
+    this.telNo = telNo;
+    this.testClient = testClient;
+    this.tlPriceLowerLimitHoliday = tlPriceLowerLimitHoliday;
+    this.tlPriceLowerLimitWeekday = tlPriceLowerLimitWeekday;
+    this.tourismClubCd = tourismClubCd;
+    this.updateModuleCd = updateModuleCd;
+    this.updateUserCd = updateUserCd;
+    this.updatedAt = updatedAt;
+    this.version = version;
+    this.verticalInterval = verticalInterval;
+    this.yardCountAll = yardCountAll;
+  }
+
+  toArray() {
+    return [
+      this.golfCourseId,
+      this.golfCourseName,
+      this.golfCourseCaption,
+      this.information,
+      this.highway,
+      this.ic,
+      this.postalCode,
+      this.address,
+      this.telephoneNo,
+      this.closeDay,
+      this.creditCard,
+      this.shoes,
+      this.dressCode,
+      this.practiceFacility,
+      this.lodgingFacility,
+      this.otherFacility,
+      this.golfCourseImageUrl1,
+      this.weekdayMinPrice,
+      this.baseWeekdayMinPrice,
+      this.holidayMinPrice,
+      this.baseHolidayMinPrice,
+      this.designer,
+      this.courseType,
+      this.courseVerticalInterval,
+      this.dimension,
+      this.green,
+      this.greenCount,
+      this.holeCount,
+      this.parCount,
+      this.ratingNum,
+      this.evaluation,
+      this.staff,
+      this.facility,
+      this.meal,
+      this.course,
+      this.costperformance,
+      this.distance,
+      this.fairway,
+      this.clubCd,
+      this.acareUsable,
+      this.accessBus,
+      this.accessCar,
+      this.accessTrain,
+      this.approvalContent,
+      this.approvalDate,
+      this.approvalFlg,
+      this.bankAccountFlg,
+      this.cancelFeeDayHoliday,
+      this.cancelFeeDayUsual,
+      this.cancelFeeFlg,
+      this.cancelNote,
+      this.card,
+      this.cardNote,
+      this.cartNote,
+      this.catchCopy,
+      this.chargeName,
+      this.chargeSCd,
+      this.chgDedlineDayReal,
+      this.chgDedlineDayReq,
+      this.chgDedlineHmReal,
+      this.chgDedlineHmReq,
+      this.clubRegistDatetime,
+      this.clubUpdateDatetime,
+      this.cnlDedlineDayReal,
+      this.cnlDedlineDayReq,
+      this.cnlDedlineHmReal,
+      this.cnlDedlineHmReq,
+      this.compeRoom,
+      this.compeRoomNote,
+      this.contractFlg,
+      this.coreSystemId,
+      this.couponClubUsable,
+      this.couponRUsable,
+      this.courseCount,
+      this.courseDesigner,
+      this.coursePresentation,
+      this.courseSize,
+      this.createModuleCd,
+      this.createUserCd,
+      this.createdAt,
+      this.deleted,
+      this.delivery,
+      this.distanceUnit,
+      this.drconHole,
+      this.dress,
+      this.faxNo,
+      this.gdoFrameFlg,
+      this.gdoPlanFlg,
+      this.greenBent,
+      this.greenKorai,
+      this.greenOther,
+      this.holeCountAll,
+      this.hotSpring,
+      this.hotSpringNote,
+      this.hotel,
+      this.id,
+      this.jalanInformation,
+      this.inquiryEigyoCd,
+      this.kana,
+      this.latitude,
+      this.longitude,
+      this.maxTeeCount,
+      this.name,
+      this.nrpinHole,
+      this.openFlg,
+      this.openPlanFlg,
+      this.openPlanMonthHoliday,
+      this.openPlanMonthHolidayFlg,
+      this.openPlanMonthSatday,
+      this.openPlanMonthSatdayFlg,
+      this.openPlanMonthUsual,
+      this.openPlanMonthUsualFlg,
+      this.openPlanTimerFlg,
+      this.openYmd,
+      this.parCountAll,
+      this.photoGalleryOpenFlg,
+      this.pointUpPostingStatus,
+      this.pointUsable,
+      this.ponpareUsable,
+      this.practiceSpace,
+      this.practiceSpaceNote,
+      this.prefectureId,
+      this.priceLowerLimitHoliday,
+      this.priceLowerLimitWeekday,
+      this.rentalClub,
+      this.rentalClubNote,
+      this.rentalShoesNote,
+      this.reserveMaxParty,
+      this.restaurantNote,
+      this.roundStyleNote,
+      this.rsvDedlineDayReal,
+      this.rsvDedlineDayReq,
+      this.rsvDedlineHmReal,
+      this.rsvDedlineHmReq,
+      this.sCd,
+      this.seq,
+      this.shoesNote,
+      this.stayPlanUsable,
+      this.telNo,
+      this.testClient,
+      this.tlPriceLowerLimitHoliday,
+      this.tlPriceLowerLimitWeekday,
+      this.tourismClubCd,
+      this.updateModuleCd,
+      this.updateUserCd,
+      this.updatedAt,
+      this.version,
+      this.verticalInterval,
+      this.yardCountAll,
+    ];
+  }
+}
